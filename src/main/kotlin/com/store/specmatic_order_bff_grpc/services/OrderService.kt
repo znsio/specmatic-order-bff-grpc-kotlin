f87618adb5a3b8ee518e63e048623dbc13823320
Sufiyan <StarKhan6368@gmail.com>
2024-06-28 11:58:51 +0530

    Switch dependency management from Maven to Gradle. - Reconfigured project build system to use Gradle - Automated compilation of proto files - Incorporated feedback from code review - Validation is Pendin

--- src/main/kotlin/com/store/specmatic_order_bff_grpc/services/OrderService.kt
+++ src/main/kotlin/com/store/specmatic_order_bff_grpc/services/OrderService.kt
@@ -1,21 +1,19 @@
 package com.store.specmatic_order_bff_grpc.services
 
-import build.buf.protovalidate.Validator
-import build.buf.protovalidate.exceptions.ValidationException
-import com.google.protobuf.GeneratedMessageV3
-import com.store.bff.proto.findAvailableProductsRequest
-import com.store.order.proto.NewOrder
-import com.store.order.proto.OrderId
+import com.store.bff.proto.*
 import com.store.order.proto.OrderServiceGrpc
-import com.store.product.proto.*
+import com.store.product.proto.ProductServiceGrpc
 import net.devh.boot.grpc.client.inject.GrpcClient
 import org.springframework.stereotype.Service
+import com.store.order.proto.NewOrder as ServiceNewOrder
+import com.store.order.proto.OrderStatus as ServiceOrderStatus
+import com.store.product.proto.NewProduct as ServiceNewProduct
+import com.store.product.proto.ProductSearchRequest as ServiceProductSearchRequest
+import com.store.product.proto.ProductType as ServiceProductType
 
 
 @Service
 class OrderService {
-
-    private final val validator = Validator()
 
     @GrpcClient("order-service")
     lateinit var orderServiceGrpc: OrderServiceGrpc.OrderServiceBlockingStub
@@ -23,27 +21,35 @@ class OrderService {
     @GrpcClient("order-service")
     lateinit var productServiceGrpc: ProductServiceGrpc.ProductServiceBlockingStub
 
-    private fun validateOrThrow(request: GeneratedMessageV3?) {
-        val result = validator.validate(request)
-        if (result.violations.isNotEmpty()) {
-            throw ValidationException(result.toString())
-        }
-    }
-
-    fun createOrder(newOrder: NewOrder?): OrderId {
-        validateOrThrow(newOrder)
-        return orderServiceGrpc.addOrder(newOrder)
-    }
-
-    fun findProducts(findAvailableProductsRequest: findAvailableProductsRequest?): ProductListResponse {
-        validateOrThrow(findAvailableProductsRequest)
-        return productServiceGrpc.searchProducts(
-            ProductSearchRequest.newBuilder().setType(findAvailableProductsRequest?.type).build()
+    fun createOrder(newOrder: NewOrder): OrderId {
+        val orderId = orderServiceGrpc.addOrder(
+            ServiceNewOrder.newBuilder().setProductid(newOrder.productid).setCount(newOrder.count).setStatus(
+                ServiceOrderStatus.PENDING
+            ).build()
         )
+        return OrderId.newBuilder().setId(orderId.id).build()
     }
 
-    fun createProduct(newProduct: NewProduct?): ProductId {
-        validateOrThrow(newProduct)
-        return productServiceGrpc.addProduct(newProduct)
+    fun findProducts(findAvailableProductsRequest: findAvailableProductsRequest): ProductListResponse {
+        val products = productServiceGrpc.searchProducts(
+            ServiceProductSearchRequest.newBuilder().setType(
+                ServiceProductType.forNumber(findAvailableProductsRequest.typeValue)
+            ).build()
+        ).productsList.map {
+            Product.newBuilder().setId(it.id).setName(it.name).setInventory(it.inventory).setType(
+                ProductType.forNumber(it.typeValue)
+            ).build()
+        }
+
+        return ProductListResponse.newBuilder().addAllProducts(products).build()
+    }
+
+    fun createProduct(newProduct: NewProduct): ProductId {
+        val productId = productServiceGrpc.addProduct(
+            ServiceNewProduct.newBuilder().setName(newProduct.name).setType(
+                ServiceProductType.forNumber(newProduct.typeValue)
+            ).setInventory(newProduct.inventory).build()
+        )
+        return ProductId.newBuilder().setId(productId.id).build()
     }
 }
