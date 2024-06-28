3885d8662785ad6e9c11a0353f78c3360ae12323
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-06-28 21:51:02 +0530

    Add contract test

---
+++ src/test/kotlin/com/store/specmatic_order_bff_grpc/ContractTest.kt
@@ -0,0 +1,32 @@
+package com.store.specmatic_order_bff_grpc
+
+import `in`.specmatic.grpc.junit.SpecmaticGrpcContractTest
+import `in`.specmatic.grpc.stub.GrpcStub
+import `in`.specmatic.grpc.utils.HOST
+import `in`.specmatic.grpc.utils.PORT
+import `in`.specmatic.grpc.utils.STUB_PORT
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContractTest : SpecmaticGrpcContractTest {
+    companion object {
+        private var grpcStub: GrpcStub? = null
+
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            System.setProperty(HOST, "localhost")
+            System.setProperty(PORT, "8080")
+            grpcStub = GrpcStub.createGrpcStub(9090)
+            grpcStub?.start()
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun tearDown() {
+            grpcStub?.stop()
+        }
+    }
+}
