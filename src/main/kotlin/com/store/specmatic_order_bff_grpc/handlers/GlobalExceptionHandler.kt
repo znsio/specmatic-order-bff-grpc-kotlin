9bf5c6d855a39c9b08580072eddcfb0f43c81da8
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-06-30 18:09:48 +0530

    Add externalised examples for required field check scenarios

--- src/main/kotlin/com/store/specmatic_order_bff_grpc/handlers/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/store/specmatic_order_bff_grpc/handlers/GlobalExceptionHandler.kt
@@ -3,6 +3,10 @@ package com.store.specmatic_order_bff_grpc.handlers
 import io.grpc.Status
 import net.devh.boot.grpc.server.advice.GrpcAdvice
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler
+import build.buf.protovalidate.exceptions.ValidationException
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.ProtoUtils
+import com.google.rpc.Status as RpcStatus
 
 @GrpcAdvice
 class GlobalGrpcExceptionHandler {
@@ -12,7 +16,17 @@ class GlobalGrpcExceptionHandler {
     }
 
     @GrpcExceptionHandler(ValidationException::class)
-    fun handleValidationException(exception: ValidationException): Status {
-        return Status.INVALID_ARGUMENT.withDescription(exception.message).withCause(exception)
+    fun handleValidationException(exception: ValidationException): StatusRuntimeException? {
+        val rpcStatus = RpcStatus.newBuilder()
+            .setCode(Status.INVALID_ARGUMENT.code.value())
+            .setMessage(exception.message ?: "Invalid argument")
+            .build()
+
+        val trailers = io.grpc.Metadata()
+        trailers.put(ProtoUtils.keyForProto(RpcStatus.getDefaultInstance()), rpcStatus)
+
+        return Status.INVALID_ARGUMENT
+            .withDescription(exception.message)
+            .asRuntimeException(trailers)
     }
 }
